a7efc6583ab39022fd8fb465eb227c443d0047e2
Abhinav Anand <abhinava32@gmail.com>
2023-05-09 10:17:36 +0530

    signUp and signIn backend code for manual authentication completed

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const usersController = require('../controllers/users_controller');
 // const createProfileController = require('../controllers/createUserController')
-
+const profileController = require('../controllers/profileController');
 
 
 // router.post('/createProfile',createProfileController.createProfile);
-router.get('/create-session', usersController.signIn);
+router.get('/sign-in', usersController.signIn);
 router.post('/create-profile', usersController.createProfile);
-router.get('/sign-up',usersController.signUp)
+router.get('/sign-up',usersController.signUp);
+router.post('/create-session', usersController.createSession);
+router.get('/profile',profileController.showProfile);
+
+
 
 module.exports = router;
